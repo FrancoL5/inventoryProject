6d56bcd0757cb45fb9e1e9571d4a24bc54085dcb
Franco Luna <francovalentin5005@gmail.com>
2024-03-08 20:40:45 -0300

    Commit para setear token

--- src/components/inventory/Inventory.tsx
+++ src/components/inventory/Inventory.tsx
@@ -3,6 +3,7 @@ import { MenuActions } from "./menuAcciones/menuActions"
 import { ShowInventory } from "./showInventory"
 
 export function Inventory() {
+    console.log()
     return (
         <Box>
             <MenuActions></MenuActions>
